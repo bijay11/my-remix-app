181d12dd22c42e51c3be58325ae4c8f3989183f6
Bijay Tamang <tmg.bijay11@gmail.com>
2024-05-26 15:53:09 -0400

    add nunito fonts

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,19 @@
+import { LinksFunction } from '@remix-run/node';
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react';
+import fontStylesUrl from './styles/font.css?url';
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: 'icon', type: 'image/svg+xml', href: './favicon.svg' },
+    { rel: 'stylesheet', href: fontStylesUrl },
+  ];
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -13,6 +22,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        {/* <link rel="icon" type="image/svg+xml" href="/favicon.svg" /> */}
         <Links />
       </head>
       <body>
