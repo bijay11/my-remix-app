5355dcb9881b2eba16f85b743983dbe30319b0e9
Bijay Tamang <tmg.bijay11@gmail.com>
2024-06-26 20:42:32 -0400

    add dynamic meta

--- app/routes/users.$username.tsx
+++ app/routes/users.$username.tsx
@@ -1,7 +1,15 @@
 import { json, type LoaderFunctionArgs } from '@remix-run/node';
-import { Link, useLoaderData } from '@remix-run/react';
+import { Link, MetaFunction, useLoaderData } from '@remix-run/react';
 import { db } from '#app/utils/db.server.js';
 import { invariantResponse } from '#app/utils/misc.js';
+
+export const meta: MetaFunction<typeof loader> = ({ data, params }) => {
+  const displayName = data?.user.name ?? params.username;
+  return [
+    { title: `${displayName} | User` },
+    { name: 'description', content: `${displayName}'s description` },
+  ];
+};
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const { username } = params;
