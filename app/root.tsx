3ea1a0fcdd4a6009e4c34e29c67802606a2c854e
Bijay Tamang <tmg.bijay11@gmail.com>
2024-06-28 11:07:00 -0400

    add error boundary on root

--- app/root.tsx
+++ app/root.tsx
@@ -8,14 +8,15 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  LiveReload,
 } from '@remix-run/react';
 import fontStylesUrl from './styles/font.css?url';
 import tailwindStylesUrl from './styles/tailwind.css?url';
 import { getEnv } from './utils/env.server';
+import { GeneralErrorBoundary } from './components/error-boundary';
 
 export const links: LinksFunction = () => {
   return [
-    // { rel: 'icon', type: 'image/svg+xml', href: './favicon.svg' },
     { rel: 'stylesheet', href: fontStylesUrl },
     { rel: 'stylesheet', href: tailwindStylesUrl },
   ];
@@ -25,36 +26,55 @@ export async function loader() {
   return json({ username: os.userInfo().username, ENV: getEnv() });
 }
 
-export function Layout({ children }: { children: React.ReactNode }) {
-  const data = useLoaderData<typeof loader>();
-
+function Document({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
-        {/* <link rel="icon" type="image/svg+xml" href="/favicon.svg" /> */}
         <Links />
       </head>
-      <body>
-        <Link to="/">Access Notes</Link>
-        <hr />
-        <div className="p-4">{children}</div>
-        <hr />
-        <Link to="/">Access Notes</Link>
+      <body className="flex h-full flex-col justify-between bg-background text-foreground">
+        {children}
         <ScrollRestoration />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.ENV=${JSON.stringify(data.ENV)}`,
-          }}
-        ></script>
         <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
 }
 
 export default function App() {
-  return <Outlet />;
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <Document>
+      <header className="container mx-auto py-6">
+        <nav className="flex justify-between">
+          <Link to="/">Access Notes</Link>
+          <Link to="/users/johndoe">John Doe</Link>
+        </nav>
+      </header>
+
+      <div className="p-4 flex-1">
+        <Outlet />
+      </div>
+
+      <div className="h-5" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.ENV = ${JSON.stringify(data.ENV)}`,
+        }}
+      />
+    </Document>
+  );
+}
+
+export function ErrorBoundary() {
+  return (
+    <Document>
+      <GeneralErrorBoundary />
+    </Document>
+  );
 }
